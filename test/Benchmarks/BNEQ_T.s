e8458e094ef9fb1f22e44bf7dda59527d43ceb61
OmarZohir <46797225+OmarZohir@users.noreply.github.com>
2019-04-06 18:56:28 -0400

    T and NNTT

---
+++ test/Benchmarks/BNEQ_T.s
@@ -0,0 +1,48 @@
+.code
+	daddi r1,r1,20 
+loop1: daddi r3,r3,234
+	daddi r2,r2,5
+	dmult r3,r2 ;
+	ddiv  r3,r2	    ;
+	daddi r1,r1,-1
+	daddi r4,r4,8	
+	daddi r5,r5,7 	
+	bneq R4, R5, loop2;
+
+loop2: daddi r2,r3,25
+	ddiv r3,r2
+	bneq R4,R5,loop3
+
+	dmult r3,r2 
+	ddiv  r3,r2	    
+	daddi r1,r1,-1
+
+loop3: daddi r2,r3,25
+	ddiv r3,r2
+	bneq R4,R5,loop4
+
+	dmult r3,r2 ; extra code
+	ddiv  r3,r2	    
+	daddi r1,r1,-1
+
+loop4: daddi r2,r3,25
+	ddiv r3,r2
+	bneq R4,R5,loop5
+
+	dmult r3,r2 ;
+	ddiv  r3,r2	    ;
+	daddi r1,r1,-1
+
+loop5: daddi r2,r3,25
+	ddiv r3,r2
+	bneq R4,R5,loop6
+
+	dmult r3,r2 ;
+	ddiv  r3,r2	    ;
+	daddi r1,r1,-1
+
+loop6: daddi r2,r3,25
+	ddiv r3,r2
+	bneq R4,R5,END
+
+	END: SYSCALL 0
